bd9782215dfe2b6a17a65159c52ef6f97bfcb09c
Arismário Neves <arismarioneves@users.noreply.github.com>
2020-11-28 14:44:04 -0300

    Add files via upload

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,69 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Location } from '@reach/router'
+import Link from 'components/Link'
+import Container from 'components/Container'
+import { Heart, Fire } from 'components/Icon'
+import Item from './Item'
+
+const StickyContainer = styled(Container)`
+	top: 0;
+	position: sticky;
+	z-index: 2;
+	background: ${p => p.theme.colors.dark};
+	margin: 0.5rem auto;
+`
+
+const Wrapper = styled.div`
+	padding: 0.75rem 0 0.75rem;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+`
+
+const Logo = styled(Link)`
+	text-decoration: none;
+	color: currentColor;
+	margin: -0.5rem;
+	padding: 0.5rem;
+	border-radius: 2rem;
+	user-select: none;
+	text-align: center;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+	vertical-align: 100px;
+	font-size: 1.75rem;
+	height: 3rem;
+	width: 3rem;
+	&:focus{${p => p.theme.focusShadow}}
+`
+
+const Flex = styled.div`
+	display: flex;
+	align-items: center;
+`
+
+const Navbar = () => (
+	<StickyContainer>
+		<Wrapper>
+			<Logo tabIndex={0} to='/'>
+				<span role='img' aria-label='movies logo'>🍿</span>
+			</Logo>
+			<Location>
+				{({location: {pathname}}) => (
+					<Flex>
+						<Item to='/featured' active={pathname === '/featured'}>
+							<Fire filled={pathname === '/featured'} style={{transform: 'translateY(1px)'}}/>
+						</Item>
+						<Item to='/favorites' active={pathname === '/favorites'}>
+							<Heart filled={pathname === '/favorites'} style={{transform: 'translateY(1px)'}}/>
+						</Item>
+					</Flex>
+				)}
+			</Location>
+		</Wrapper>
+	</StickyContainer>
+)
+
+export default Navbar
---
+++ src/components/SearchView/CardsByPage.js
@@ -0,0 +1,66 @@
+import React, { Fragment, useEffect } from 'react'
+import { useFetch } from 'react-hooks-fetch'
+import { useInView } from 'react-intersection-observer'
+import { Cell } from 'griding'
+import { getKindByURL } from 'utils/kind'
+import Card from 'components/Card'
+import InfoScreen from 'components/InfoScreen'
+
+const getDataURL = (kindURL, search, page) => {
+	const base = 'https://api.themoviedb.org/3'
+	const api = process.env.REACT_APP_TMDB_KEY
+	if(kindURL === 'featured') return ([
+		`${base}/discover/movie`,
+		`?api_key=${api}`,
+		`&page=${page}`
+	])
+	return ([
+		`${base}/search/${getKindByURL(kindURL)}`,
+		`?api_key=${api}`,
+		`&query=${search}`,
+		`&page=${page}`,
+	])
+}
+
+const InfiniteScroll = ({page, setPage}) => {
+	useEffect(() => setPage(page + 1))
+	return null
+}
+
+const CardsByPage = ({search, page, setPage, isLastPage, kindURL}) => {
+	const { loading, data, error } = useFetch(getDataURL(kindURL, search, page).join(''))
+	const [ref, inView] = useInView()
+
+	if(error && search) return (
+		<Cell xs={12}><InfoScreen emoji='❌' title='I’m sorry Dave' description='I’m afraid i can’t do that'/></Cell>
+	)
+
+	if(loading) return (Array(20).fill(0).map((x, i) => (
+		<Cell key={i} xs={6} sm={4} md={3} xg={2}><Card key={i} loading/></Cell>
+	)))
+
+	if(data && !data?.results?.length) return (
+		<Cell xs={12}><InfoScreen emoji='😕' title={`No results found for ${search}`} description='let’s try another one'/></Cell>
+	)
+
+	if(!data?.results?.length) return null
+
+	const totalPages = data?.total_pages
+	return (
+		<Fragment>
+			{data?.results?.map(entry => (
+				<Cell key={entry.id} xs={6} sm={4} md={3} xg={2}>
+					<Card kindURL={kindURL === 'featured' ? 'movies' : kindURL} {...entry}/>
+				</Cell>
+			))}
+			{isLastPage && totalPages && totalPages > page && (
+				<Cell xs={6} sm={4} md={3} xg={2}>
+					<Card onClick={() => setPage(page + 1)} loadMore/>
+					{page > 1 && <div ref={ref}>{inView && <InfiniteScroll page={page} setPage={setPage}/>}</div>}
+				</Cell>
+			)}
+		</Fragment>
+	)
+}
+
+export default CardsByPage
