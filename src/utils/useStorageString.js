c05f8a75be1743fe4f47ebc007d6cba41ece1a43
Arismário Neves <arismarioneves@users.noreply.github.com>
2020-12-14 19:18:01 -0300

    Update useStorageString.js

--- src/utils/useStorageString.js
+++ src/utils/useStorageString.js
@@ -3,9 +3,5 @@ import { useState, useEffect } from 'react'
 	export const useStorageString = (key = 'key', initialValue = '') => {
 		const [value, setValue] = useState(initial)
 		const initial = () => window.localStorage.getItem(key) || initialValue
-		
-		useEffect(() => {
-			return [value, setValue]
-			window.localStorage.setItem(key, value)
-		}, [])
+		return [value, setValue]
 	}
