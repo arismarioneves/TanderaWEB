58c3078c768b743ca3b8cf2844243d0f1b0837a5
Arismário Neves <arismarioneves@users.noreply.github.com>
2020-12-14 19:17:14 -0300

    Update index.js

--- src/components/DetailView/index.js
+++ src/components/DetailView/index.js
@@ -63,45 +63,41 @@ const DetailView = ({id, kindURL, ...props}) => {
 	const image = data?.poster_path || data?.profile_path
 	const imdb = data?.imdb_id || data?.external_ids?.imdb_id
 	const score = data?.vote_average
-
-	useEffect(() => {
 	
-		return(
-			<Wrapper error={error}>
-				<Container>
-					{loading && <Loader/>}
-					{!loading && data && (
-						<Row vertical-gutter style={{justifyContent: 'space-between'}}>
-							<Cell xs={12} md={6} style={{marginBottom: '1.5rem'}}>
-								<BackLink onClick={() => window.history.back()}><Arrow/></BackLink>
-								<Meta {...data}/>
-								<Text weight={600} xs={2} sm={3} md={4} xg={5}>{title}</Text>
-								<div style={{display: 'flex', margin: '1rem -0.5rem'}}>
-									{!!imdb && (
-										<Button imdb={imdb} background='#FF9F1C' logo={<IMDB color='#0A1014'/>}>
-											{!!score && `${score}/10`}
-										</Button>
-									)}
-									<ToggleButton kindURL={kindURL} id={id}/>
-								</div>
-								<Info kind={kind} {...data}/>
-							</Cell>
-							<Cell xs={12} sm={12} md={5} lg={5}>
-								<Image kind={kind} alt={`poster for: ${title}`} image={image}/>
-							</Cell>
-						</Row>
-					)}
-				</Container>
-				{error && (
-					<Container style={{flex: 1}}>
-						<InfoScreen emoji='❌' title='I’m sorry Dave' description='I’m afraid i can’t do that'/>
-					</Container>
+	return(
+		<Wrapper error={error}>
+			<Container>
+				{loading && <Loader/>}
+				{!loading && data && (
+					<Row vertical-gutter style={{justifyContent: 'space-between'}}>
+						<Cell xs={12} md={6} style={{marginBottom: '1.5rem'}}>
+							<BackLink onClick={() => window.history.back()}><Arrow/></BackLink>
+							<Meta {...data}/>
+							<Text weight={600} xs={2} sm={3} md={4} xg={5}>{title}</Text>
+							<div style={{display: 'flex', margin: '1rem -0.5rem'}}>
+								{!!imdb && (
+									<Button imdb={imdb} background='#FF9F1C' logo={<IMDB color='#0A1014'/>}>
+										{!!score && `${score}/10`}
+									</Button>
+								)}
+								<ToggleButton kindURL={kindURL} id={id}/>
+							</div>
+							<Info kind={kind} {...data}/>
+						</Cell>
+						<Cell xs={12} sm={12} md={5} lg={5}>
+							<Image kind={kind} alt={`poster for: ${title}`} image={image}/>
+						</Cell>
+					</Row>
 				)}
-			</Wrapper>
-		)
-	
-		document.title = getDetailTitle(kindURL, title)
-	}, [data])
+			</Container>
+			{error && (
+				<Container style={{flex: 1}}>
+					<InfoScreen emoji='❌' title='I’m sorry Dave' description='I’m afraid i can’t do that'/>
+				</Container>
+			)}
+		</Wrapper>
+	)
+
 }
 
 export default DetailView
